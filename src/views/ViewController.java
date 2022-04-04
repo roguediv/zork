84c2bef6807015b633d9636c72301af34b4de6a1
Jake <jmirandam998@gmail.com>
2022-04-03 21:31:25 -0400

    Revert

--- src/views/ViewController.java
+++ src/views/ViewController.java
@@ -104,7 +104,7 @@ public class ViewController {
     //   Documentation: https://docs.oracle.com/javase/7/docs/api/java/awt/font/LineBreakMeasurer.html
     // * Deal with how bad <html> looks and have the user use horizontal scroll to see text. [3/26]
     //Label temp = new Label(string, stkText.view);
-    new Label("<html><p>"+string+"</p></html>", stkText.view);
+    createMessageLabel(string);
     reload();
   }
   public void sendText(String[] strings) {
@@ -119,7 +119,7 @@ public class ViewController {
     }
     reload();
   }
-  //test
+  
   private void createMessageLabel(String string) {
     new Label("<html><p>"+string+"</p></html>", stkText.view);
   }
