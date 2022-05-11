b9e06f93b61ae66528de77a1ffd04c1ce10c8c57
bjherman949 <bjherman949@hotmail.com>
2022-05-11 03:02:36 -0500

    "Updated Dialogue and added comments"

--- src/classes/instances/locations/environments/overworld_environments/Palace.java
+++ src/classes/instances/locations/environments/overworld_environments/Palace.java
@@ -9,15 +9,15 @@ import src.classes.instances.locations.environments.underground_environments.Jai
 public class Palace extends Environment {
 
   public Palace(String Name, Environment Source) {
-    super(Name, Source, new String[] {"You have entered the royal palace."});
+    super(Name, Source, new String[] {"You have entered the royal palace.",
+    "You get closer to the Palace and are approached by a guard."});
 
-    // You get closer to the Palace and are approached by a guard.
-    // "Hault, what is your reason for entering the palace?"
-
+    // Create a new enemy and a new weapon. Equip the weapon to the enemy.
     Enemy palaceGuard = new Enemy("guard", 100.00, 50.00, this);
     SharpObject ironSword = new SharpObject("iron_sword", 100.00, 13);
     palaceGuard.equipNewItem(ironSword);
 
+    // Get the king and change his location. create a new weapon and equip it to the King.
     Enemy king = entityManager.getEnemies().getInstance("king_ethelred");
     king.setLocation(this);
     SharpObject dagger = new SharpObject("King's sword", 0.00, 25.00);
@@ -25,6 +25,7 @@ public class Palace extends Environment {
 
     Player player = (Player) entityManager.getEntity("player");
 
+    // Create new dialogue and passes into a ChangeDialgoue method.
     String[][][][] dialogueChange = {
     {
       {
@@ -32,49 +33,77 @@ public class Palace extends Environment {
       },
       {
           {"I'm curious to see what the inside looks like."},
+          {"I'm here to kill the King!"},
           {"Leave"}
       }
     },
     {
-      // After option 1 is picked
       {
-        {"What a bad reason, I'll gladly show you the inside of a cell."}
-      },
-      {
-        {"*Start Combat with guard*",
-          "(Type attack guard)"}
+        {
+          "What a bad reason, I'll gladly show you the inside of a cell.",
+         "*Start Combat with guard*",
+          "(Type attack guard when conversation is over.)"
+        }
       }
     },
     {
       {
+        {
+          "The king happends to be close by and is eager for a challenge",
+          "(Type attack king ethelred conversation is over.)"
+        },{
+          "The king happends to be close by and is eager for a challenge",
+          "(Type attack king ethelred conversation is over.)"
+        },{
+          "The king happends to be close by and is eager for a challenge",
+          "(Type attack king ethelred conversation is over.)"
+        }
+      }
+    },
+    {
+      {
+        {"Fine leave!"},
+        {"Fine leave!"},
         {"Fine leave!"}
       }
     }
   };
 
+  // Change the properties for the new dialogue options.
   int[][][] propertiesChange = {
     {
       {
-        1,1,0,0
+        1
       },
       {
-        1,2,2,2
+        1,2,3
       }
     },
     {
       {
-        1,1,0,0
+        0, 0, 0, 0
       },
       {
-        2,2,2,2
+        0
       }
     },
     {
       {
-        0,0,0,0
+        0, 0, 0, 0
+      },
+      {
+        0
+      }
+    },   
+    {
+      {
+        0, 0, 0, 0
       }
     }
   };
+
+  // Use the dialogue and properties variable created above and change the
+  // the new enemys dialogue and properties.
   palaceGuard.changeDialog(dialogueChange); 
   palaceGuard.changeProperties(propertiesChange);
 
--- src/classes/instances/locations/environments/overworld_environments/Tavern.java
+++ src/classes/instances/locations/environments/overworld_environments/Tavern.java
@@ -1,5 +1,7 @@
 package src.classes.instances.locations.environments.overworld_environments;
 
+import java.time.LocalTime;
+
 import src.classes.instances.entities.Enemy;
 import src.classes.instances.entities.Merchant;
 import src.classes.instances.locations.environments.Environment;
@@ -7,19 +9,20 @@ import src.classes.instances.locations.environments.Environment;
 public class Tavern extends Environment {
 
   public Tavern(String Name, Environment Source) {
-    super(Name, Source, new String[] {"Tavern Entered"});
-
-    // "When you enter the tavern you see a crowd of villagers looking for a fight.",
-    // "A bartender with the right amount of coin he could tell you somthing important."
-
+    super(Name, Source, new String[] {"When you enter the tavern you see a crowd of villagers looking for a fight.",
+    "A bartender with the right amount of coin he could tell you somthing important."});
+    
+    // Create a villager enemy and a bartender merchant
     Enemy tavernVillager = new Enemy("villager", 10.00, 50.00, this);
     Merchant bartender = new Merchant("bartender", 50.00, 10.00);
     
+    // Populate the Bartenders shop
     addEntity(bartender);
     bartender.addItem(itemManager.getItem("beer"));
     bartender.addItem(itemManager.getItem("broken_bottle"));
     bartender.addItem(itemManager.getItem("information"));
 
+    // Create new dialogue and passes into a ChangeDialgoue method.
     String[][][][] dialogueChange = {
         {
             {
@@ -46,6 +49,7 @@ public class Tavern extends Environment {
         }
     };
 
+    // Change the properties for the new dialogue options.
     int[][][] propertiesChange = {
         {
             {
@@ -70,6 +74,8 @@ public class Tavern extends Environment {
           }
     };
     
+    // Use the dialogue and properties variable created above and change the
+    // the new enemys dialogue and properties.
     tavernVillager.changeDialog(dialogueChange); 
     tavernVillager.changeProperties(propertiesChange);
  }
